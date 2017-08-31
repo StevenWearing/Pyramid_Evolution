1bdc632ea2708bc70040236c20df984df73a4ead
StevenWearing <stewearing2@gmail.com>
2017-08-31 04:30:53 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,178 @@
+
+//       Introduction Buttons
+//          CHECK BOTTOM -190
+
+$("#introButtonSB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#intro").offset().top -100
+    }, 2000);
+});
+
+$("#introButtonCB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#intro").offset().top -100
+    }, 2000);
+});
+
+$("#introButtonMB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#intro").offset().top -100
+    }, 2000);
+});
+
+$("#introButtonStepP").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#intro").offset().top -100
+    }, 2000);
+});
+
+$("#introButtonSnefP").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#intro").offset().top -100
+    }, 2000);
+});
+
+$("#introButtonGP").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#intro").offset().top -100
+    }, 2000);
+});
+
+$("#introButtonLB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#intro").offset().top -100
+    }, 2000);
+});
+
+
+
+//          Sand Burial
+
+$("#sandBurialButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#sandBurial").offset().top -100
+    }, 2000);
+});
+
+$("#sandBurialButtonCB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#sandBurial").offset().top -100
+    }, 2000);
+});
+
+
+
+//        Coffin Burial
+
+$("#coffinBurialButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#coffinBurial").offset().top -100
+    }, 2000);
+});
+
+$("#coffinBurialButtonSB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#coffinBurial").offset().top -100
+    }, 2000);
+});
+
+$("#coffinBurialButtonMB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#coffinBurial").offset().top -100
+    }, 2000);
+});
+
+
+
+//         Mastaba Burial
+
+$("#mastabaBurialButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#mastabaBurial").offset().top -40
+    }, 2000);
+});
+
+$("#mastabaBurialButtonCB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#mastabaBurial").offset().top -40
+    }, 2000);
+});
+
+$("#mastabaBurialButtonStepP").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#mastabaBurial").offset().top -40
+    }, 2000);
+});
+
+
+
+//         Step Pyramid
+
+$("#stepPyramidButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#stepPyramid").offset().top -100
+    }, 2000);
+});
+
+$("#stepPyramidButtonMB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#stepPyramid").offset().top -100
+    }, 2000);
+});
+
+$("#stepPyramidButtonSnefP").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#stepPyramid").offset().top -100
+    }, 2000);
+});
+
+
+
+//         Snefru's Pyramids
+
+$("#snefruPyramidButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#snefruPyramid").offset().top -20
+    }, 2000);
+});
+
+$("#snefruPyramidButtonStepP").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#snefruPyramid").offset().top -20
+    }, 2000);
+});
+
+$("#snefruPyramidButtonGP").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#snefruPyramid").offset().top -20
+    }, 2000);
+});
+
+
+
+//         Great Pyramid
+
+$("#greatPyramidButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#greatPyramid").offset().top -20
+    }, 2000);
+});
+
+$("#greatPyramidButtonSnefP").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#greatPyramid").offset().top -20
+    }, 2000);
+});
+
+$("#greatPyramidButtonLB").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#greatPyramid").offset().top -20
+    }, 2000);
+});
+
+
+
+//     More information
+$("#infoButton").click(function() {
+  $('html, body').animate({scrollTop: $('#info').offset().top}, 2000);
+});
